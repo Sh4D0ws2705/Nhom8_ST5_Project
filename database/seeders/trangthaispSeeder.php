318014e5062f1be5cdc8c8a136530bcfbefe772a
Sh4D0ws2705 <nguyenhieunghia2004@gmail.com>
2024-04-01 20:00:35 +0700

    db_trangThaiSP

---
+++ database/seeders/trangthaispSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class trangthaispSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('trangthaisp')->insert([
+            ['MaTrangThai'=>'CH01',
+            'TrangThai'=>'Còn Hàng'],
+            ['MaTrangThai'=>'HH01',
+            'TrangThai'=>'Hết Hàng'],
+            ['MaTrangThai'=>'NB01',
+            'TrangThai'=>'Ngừng Bán'],
+        ]);
+    }
+}
